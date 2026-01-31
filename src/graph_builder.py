96498d6316cf0656a9ebcc79e7aefe19b3f92171
vp-v <vyshnavi.vp99@gmail.com>
2026-01-31 17:53:53 +1100

    Updated the code for building the graph, cleaned up the code to add data types.

--- src/graph_builder.py
+++ src/graph_builder.py
@@ -5,10 +5,13 @@ import pandas as pd
 
 # Function to build a graph from a DataFrame
 # Using  Networkx to create a undirected graph
-def build_graph(layout_file):
-    df = pd.read_csv(layout_file)
-    G = nx.Graph()
+def build_graph(layout_file:str) -> nx.Graph:
 
+    df = pd.read_csv(layout_file)
+    required = {"from", "to", "distance"}
+    if not required.issubset(df.columns):
+        raise ValueError(f"layout_file must contain the following columns: {required}. Found:{set(df.columns)}")
+    G = nx.Graph()
     for _, row in df.iterrows():
-        G.add_edge(row["from"], row["to"], weight=row["distance"])
+        G.add_edge(str(row["from"]), str(row["to"]), weight=float(row["distance"]))
     return G
